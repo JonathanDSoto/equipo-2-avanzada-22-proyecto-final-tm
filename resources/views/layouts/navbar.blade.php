8db5fc53ed025964521f2249b9e695fdd95fe3aa
Bilobatetonto <cris_misterio10@hotmail.com>
2022-11-28 16:36:23 -0700

    nombre de usuario actual en el navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -53,7 +53,7 @@
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <img class="rounded-circle header-profile-user" src="{{asset('images/users/avatar-1.jpg')}}"
                             alt="Header Avatar">
-                        <span class="d-none d-xl-inline-block ms-1" key="t-henry">Henry</span>
+                        <span class="d-none d-xl-inline-block ms-1" key="t-henry">{{ Auth::User()?->name }}</span>
                         <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
                     </button>
                     <div class="dropdown-menu dropdown-menu-end">
