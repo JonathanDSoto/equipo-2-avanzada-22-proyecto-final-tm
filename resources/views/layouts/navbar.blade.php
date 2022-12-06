25e8b317de9a9033f8e975e4ca57fdb1cce878cc
Bilobatetonto <cris_misterio10@hotmail.com>
2022-12-05 18:13:27 -0700

    imagen de usuario en navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -56,8 +56,11 @@
                 <div class="dropdown d-inline-block">
                     <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img class="rounded-circle header-profile-user" src="{{asset('images/users/avatar-1.jpg')}}"
-                            alt="Header Avatar">
+                    @if( Auth::User()->avatar!=null)
+                        <img class="rounded-circle header-profile-user" src="{{Auth::User()->avatar}}"alt="Header Avatar">
+                    @else
+                        <img class="rounded-circle header-profile-user" src="{{asset('images/users/avatar-1.jpg')}}"alt="Header Avatar">
+                    @endif
                         <span class="d-none d-xl-inline-block ms-1" key="t-henry">{{ Auth::User()?->name }}</span>
                         <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
                     </button>
